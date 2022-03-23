d0fd3758d11e9460f165e95979f594b6928ee256
Zebra <zebrv.apps@gmail.com>
2022-03-23 13:42:12 +0100

    init backend project

---
+++ models/test.js
@@ -0,0 +1,13 @@
+// const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema;
+
+// const TestModelSchema = new Schema({
+//   thing: {
+//     type: String,
+//     required: true,
+//   },
+//   otherThing: { type: String, required: true },
+// });
+
+// module.exports = mongoose.model("test", TestModelSchema);
