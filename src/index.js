add70610d779bddcb5cb83f763116c164f027761
Zebra <zebrv.apps@gmail.com>
2022-03-23 18:44:44 +0100

    move to src/ and configure linter

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 //import routes
 require("./routes/testRoute.js")(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
