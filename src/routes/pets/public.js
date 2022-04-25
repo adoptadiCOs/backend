5a6346d8cc29cab127525ca9bc79b2e59dcb82fc
AndoniSalcedo <andonisalcedo@gmail.com>
2022-04-25 13:11:03 +0200

    actualizado docs

--- src/routes/pets/public.js
+++ src/routes/pets/public.js
@@ -44,14 +44,6 @@ const pets = require("../../controllers/pets.controller");
  *                      items:
  *                        type: object
  *                        properties:
- *                          date:
- *                            type: integer
- *                            description: The user ID.
- *                            example: 0
- *                          id:
- *                              type: integer
- *                              description: Pet ID
- *                              example: 4477
  *                          name:
  *                              type: string
  *                              description: Pet name
@@ -68,38 +60,9 @@ const pets = require("../../controllers/pets.controller");
  *                              type: string
  *                              description: Pet sex
  *                              example: Hembra
- *                          size:
- *                              type: string
- *                              description: average pet size
- *                              example: Mediano (11-25 kg)
- *                          color:
- *                              type: string
- *                              description: Pet color
- *                              example: BLANCO
  *                          photo:
  *                              type: string
  *                              description: url of a animal photo
- *                          description:
- *                              type: string
- *                              description: Pet description
- *                          rage:
- *                              type: boolean
- *                              description: true if pet has rage
- *                              example: false
- *                          danger:
- *                              type: boolean
- *                              description: true if pet is dangerous
- *                              example: false
- *                          sterile:
- *                              type: boolean
- *                              description: true if pet if sterile
- *                              example: false
- *                          bornDate:
- *                              type: date
- *                              description: pet born day
- *                          adoptionDate:
- *                              type: date
- *                              description: pet adoption starting day
  *              400:
  *                  description: something went wrong return error msg
  */
