fdc111c1c4d71499b8d049a4f5e68148b3cf2d9a
José Marín Díez <josemarindiez@gmail.com>
2022-05-09 12:43:24 +0200

    Sacar bbdd de index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require("./utils/env");
+require("./utils/db");
 
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
 const swaggerUi = require("swagger-ui-express");
@@ -13,37 +13,20 @@ const app = express();
 
 //Configurations
 const PORT = process.env.PORT || 8080;
-const DB = process.env.DB || "mongodb://localhost:27017/animaliCOs";
 
 // Logging
 app.use(morgan("dev"));
 
-// Swagger UI
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-//Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Swagger UI
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // API Routes
 app.use("/api", router);
 
-//app.use('/account',public_account)
-//app.use('/chat',public_chat)
-//.....
-
-//DB Connection
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.once("open", () => {
-  console.log("DB connected");
-});
-
 //Jobs
-
 const sync = require("./jobs/pets.job");
 
 sync.start();
