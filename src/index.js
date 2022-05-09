a74d54059d73df9fb03916c84876dd34975a0a32
José Marín Díez <josemarindiez@gmail.com>
2022-05-09 12:37:31 +0200

    Saca rutas de index

--- src/index.js
+++ src/index.js
@@ -1,42 +1,32 @@
-const express = require("express");
 require("./utils/env");
+
+const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
 const swaggerUi = require("swagger-ui-express");
+const router = require("./routes");
 
 const swaggerSpec = require("./utils/swagger");
 
 const app = express();
 
-
 //Configurations
-console.log(process.env.SECRET);
 const PORT = process.env.PORT || 8080;
 const DB = process.env.DB || "mongodb://localhost:27017/animaliCOs";
 
 // Logging
 app.use(morgan("dev"));
+
 // Swagger UI
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-//Import Routes
-const routes = require("./routes");
-const public_pets = require("./routes/pets/public");
-const private_pets = require("./routes/pets/private");
-const public_statistics = require("./routes/statistics/public");
-
-//Import Middlewares
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
 
-//TODO: private routes must have AuthMiddleware before the router
-app.use("/api", routes);
-app.use("/api", public_pets);
-app.use("/api", private_pets);
-app.use("/api", public_statistics);
+// API Routes
+app.use("/api", router);
 
 //app.use('/account',public_account)
 //app.use('/chat',public_chat)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 const Router = require("express");
 
 const userRoutes = require("./users.routes");
+const public_pets = require("./pets/public");
+const private_pets = require("./pets/private");
+const public_statistics = require("./statistics/public");
 
 const router = Router();
 
 router.use("/users", userRoutes);
+router.use("/", public_pets);
+router.use("/", private_pets);
+router.use("/", public_statistics);
 
 module.exports = router;
