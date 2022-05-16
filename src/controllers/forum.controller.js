45acdf6c5774634b5e192b91df187ba09a8bc566
Tomenos18 <tomas.pel.ben@gmail.com>
2022-05-16 14:17:59 +0200

    controlador de errores de foros duplicados

--- src/controllers/forum.controller.js
+++ src/controllers/forum.controller.js
@@ -1,17 +1,26 @@
 const forumHelper = require("../helpers/forum.helper");
 
 const newForum = async (req, res) => {
-  const { user, category, title, user_explanation } = req.body;
+  const { username, category, title, user_explanation } = req.body;
 
-  if (!user || !title || !user_explanation) {
+  if (!username || !title || !user_explanation) {
     return res.status(400).json({ error: "Unspecified some parameters" });
+  }
+
+  var prev = await forumHelper.getSubForum(username, title);
+  var len = prev.length; 
+
+  if (len !== 0){
+    return res
+      .status(409)
+      .send({ error: "This forum was previously created by the same user"});
   }
 
   try {
     if (category === undefined) {
-      await forumHelper.createSubForumWithoutCat(user, title, user_explanation);
+      await forumHelper.createSubForumWithoutCat(username, title, user_explanation);
     } else {
-      await forumHelper.createSubForum(user, category, title, user_explanation);
+      await forumHelper.createSubForum(username, category, title, user_explanation);
     }
     return res.status(201).json({ message: "Forum created" });
   } catch (error) {
