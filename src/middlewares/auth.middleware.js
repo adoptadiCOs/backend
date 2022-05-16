9aa654584a8079d6ef928da845a5ea28bfc2792e
José Marín Díez <josemarindiez@gmail.com>
2022-05-16 16:13:29 +0200

    Utiliza return next() en lugar de next() en los middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -18,7 +18,7 @@ const verifyToken = (req, res, next) => {
     // Añade al cuerpo los valores decodificados
     req.body = { ...req.body, username: decoded.username, id: decoded.id };
 
-    next();
+    return next();
   } catch (error) {
     return res.status(401).json({ error: "Token inválido" });
   }
@@ -30,8 +30,9 @@ const isAdmin = async (req, res, next) => {
   const user = await userHelper.findUserById(id);
 
   if (user.role === "admin") {
-    next();
+    return next();
   }
+
   return res.status(403).send({ error: "Requiere rol de administrador" });
 };
 
