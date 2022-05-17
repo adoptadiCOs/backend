a6ee2019823efb15ae0a292c24a57dead54e3c46
Tomenos18 <tomas.pel.ben@gmail.com>
2022-05-17 11:44:23 +0200

    swagger category

--- src/routes/forum/public.js
+++ src/routes/forum/public.js
@@ -4,13 +4,14 @@ const ForumController = require("../../controllers/forum.controller");
 
 const router = Router();
 
+/* List all enabled forums */
 /**
  * @swagger
  * /forum/list:
  *  post:
  *    tags:
  *      - forum
- *    summary: List active forums
+ *    summary: List forums
  *    description:
  *    consumes:
  *      - "application/json"
@@ -33,6 +34,44 @@ const router = Router();
 
 router.get("/list", ForumController.listSubForum);
 
+/**
+ * @swagger
+ * /forum/category:
+ *  post:
+ *    tags:
+ *      - forum
+ *    summary: List forums by a category
+ *    description:
+ *    consumes:
+ *      - "application/json"
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - name: category
+ *        in: body
+ *        required: true
+ *        schema:
+ *          type: object
+ *          properties:
+ *            category:
+ *              type: string
+ *          example:
+ *            category: "cats"
+ *    responses:
+ *      201:
+ *        description: A list of enabled forums
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              example: 
+ *                - user: "user1"
+ *                  title: "title1"
+ *                  user_explanation: "description1"
+ *                  category: "cats"
+ *      500:
+ *        description: Error of the server
+ */
 router.get("/category", ForumController.listSubForumByCategory);
 
 router.get("/", ForumController.getSubForum);
