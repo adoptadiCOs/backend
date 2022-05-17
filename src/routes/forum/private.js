9e81de788959323713edc7f9c6d7f0976cfe5bd3
Tomenos18 <tomas.pel.ben@gmail.com>
2022-05-17 12:18:32 +0200

    delete forum swagger

--- src/routes/forum/private.js
+++ src/routes/forum/private.js
@@ -95,8 +95,88 @@ router.post("/new", ForumController.newForum);
  */
 router.post("/reply", ForumController.addComment);
 
+/**
+ * @swagger
+ * /forum/admin/delete:
+ *  post:
+ *    tags:
+ *      - forum
+ *    summary: Delete forum as admin
+ *    description:
+ *    consumes:
+ *      - "application/json"
+ *    produces:
+ *    - application/json
+ *    parameters:
+ *      - name: access-token
+ *        in: header
+ *        required: true
+ *      - name: forum
+ *        in: body
+ *        description: Fields to update
+ *        required: false
+ *        schema:
+ *          type: object
+ *          properties:
+ *            name:
+ *              type: string
+ *            title:
+ *              type: string
+ *          example:
+ *            name: "user1"
+ *            title: "title1"
+ *    responses:
+ *      201:
+ *        description: Forum deleted
+ *      400:
+ *        description: No enough parameters
+ *      409:
+ *        description: Forum does not exits
+ *      500:
+ *        description: Error of the server
+ */
 router.delete("/admin/delete", isAdmin, ForumController.deleteSubForumAdmin);
 
+/**
+ * @swagger
+ * /forum/delete:
+ *  post:
+ *    tags:
+ *      - forum
+ *    summary: Delete forum as regular user (only can be deleted forums that the user owns)
+ *    description:
+ *    consumes:
+ *      - "application/json"
+ *    produces:
+ *    - application/json
+ *    parameters:
+ *      - name: access-token
+ *        in: header
+ *        required: true
+ *      - name: forum
+ *        in: body
+ *        description: Fields to update
+ *        required: false
+ *        schema:
+ *          type: object
+ *          properties:
+ *            name:
+ *              type: string
+ *            title:
+ *              type: string
+ *          example:
+ *            name: "user1"
+ *            title: "title1"
+ *    responses:
+ *      201:
+ *        description: Forum deleted
+ *      400:
+ *        description: No enough parameters
+ *      409:
+ *        description: Forum does not exits
+ *      500:
+ *        description: Error of the server
+ */
 router.delete("/delete", ForumController.deleteSubForum);
 
 router.delete("/admin/deletereply", isAdmin, ForumController.deleteCommentAdmin);
